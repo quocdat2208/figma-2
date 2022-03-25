63fdc44b363b01ccb26a21a320b19a144b53705c
quocdat2208 <62021124+quocdat2208@users.noreply.github.com>
2022-03-25 23:10:05 +0700

    update en/vi search paging

--- main.js
+++ main.js
@@ -1,220 +1,407 @@
-var arr = [
-    {
-        name: 'nfv01',
-        infor1:'1vCPU x86',
-        infor2:'1TB HDD',
-        infor3:'1vCPU x86',
-        infor4: '01',
-    },
-    {
-        name: 'nfv02',
-        infor1:'2vCPU x86',
-        infor2:'2TB HDD',
-        infor3:'2vCPU x86',
-        infor4: '02',
-        
-    },
-    {
-        name: 'nfv03',
-        infor1:'3vCPU x86',
-        infor2:'3TB HDD',
-        infor3:'3vCPU x86',
-        infor4: '03',
-        
-    },
-    {
-        name: 'nfv04',
-        infor1:'4vCPU x86',
-        infor2:'4TB HDD',
-        infor3:'4vCPU x86',
-        infor4: '04',
-        
-    },
-    {
-        name: 'nfv05',
-        infor1:'5vCPU x86',
-        infor2:'5TB HDD',
-        infor3:'5vCPU x86',
-        infor4: '05',
-        
-    },
-    {
-        name: 'nfv06',
-        infor1:'6vCPU x86',
-        infor2:'6TB HDD',
-        infor3:'6vCPU x86',
-        infor4: '06',
-        
-    },
-    {
-        name: 'nfv07',
-        infor1:'7vCPU x86',
-        infor2:'7TB HDD',
-        infor3:'7vCPU x86',
-        infor4: '07',
-        
-    },
-    {
-        name: 'nfv08',
-        infor1:'6vCPU x88',
-        infor2:'8TB HDD',
-        infor3:'8vCPU x86',
-        infor4: '08',
-        
-    },
-    {
-        name: 'nfv09',
-        infor1:'9vCPU x86',
-        infor2:'9TB HDD',
-        infor3:'9vCPU x86',
-        infor4: '09',
-        
-    },
-    {
-        name: 'nfv10',
-        infor1:'1vCPU x88',
-        infor2:'1TB HDD',
-        infor3:'1vCPU x86',
-        infor4: '10',
-        
-    },
+var cards = [
     
+  {
+    name: "nfvo1",
+    infor1: "1vCPU x86",
+    iconInfor1: "./asset/img/2.png",
+    infor2: "1TB HDD",
+    iconInfor2: "./asset/img/4.png",
+    infor3: "1vCPU x86",
+    iconInfor3: "./asset/img/1.png",
+    infor4: "01",
+    iconInfor4: "./asset/img/3.png",
+  },
+  {
+    name: "nfvo2",
+    infor1: "1vCPU x86",
+    iconInfor1: "./asset/img/2.png",
+    infor2: "1TB HDD",
+    iconInfor2: "./asset/img/4.png",
+    infor3: "1vCPU x86",
+    iconInfor3: "./asset/img/1.png",
+    infor4: "01",
+    iconInfor4: "./asset/img/3.png",
+  },
+  {
+    name: "nfvo3",
+    infor1: "1vCPU x86",
+    iconInfor1: "./asset/img/2.png",
+    infor2: "1TB HDD",
+    iconInfor2: "./asset/img/4.png",
+    infor3: "1vCPU x86",
+    iconInfor3: "./asset/img/1.png",
+    infor4: "01",
+    iconInfor4: "./asset/img/3.png",
+  },
+  {
+    name: "nfvo4",
+    infor1: "1vCPU x86",
+    iconInfor1: "./asset/img/2.png",
+    infor2: "1TB HDD",
+    iconInfor2: "./asset/img/4.png",
+    infor3: "1vCPU x86",
+    iconInfor3: "./asset/img/1.png",
+    infor4: "01",
+    iconInfor4: "./asset/img/3.png",
+  },
+  {
+    name: "nfvo5",
+    infor1: "1vCPU x86",
+    iconInfor1: "./asset/img/2.png",
+    infor2: "1TB HDD",
+    iconInfor2: "./asset/img/4.png",
+    infor3: "1vCPU x86",
+    iconInfor3: "./asset/img/1.png",
+    infor4: "01",
+    iconInfor4: "./asset/img/3.png",
+  },
+  {
+    name: "nfvo6",
+    infor1: "1vCPU x86",
+    iconInfor1: "./asset/img/2.png",
+    infor2: "1TB HDD",
+    iconInfor2: "./asset/img/4.png",
+    infor3: "1vCPU x86",
+    iconInfor3: "./asset/img/1.png",
+    infor4: "01",
+    iconInfor4: "./asset/img/3.png",
+  },
+  {
+    name: "nfvo7",
+    infor1: "1vCPU x86",
+    iconInfor1: "./asset/img/2.png",
+    infor2: "1TB HDD",
+    iconInfor2: "./asset/img/4.png",
+    infor3: "1vCPU x86",
+    iconInfor3: "./asset/img/1.png",
+    infor4: "01",
+    iconInfor4: "./asset/img/3.png",
+  },
+  {
+    name: "nfvo8",
+    infor1: "1vCPU x86",
+    iconInfor1: "./asset/img/2.png",
+    infor2: "1TB HDD",
+    iconInfor2: "./asset/img/4.png",
+    infor3: "1vCPU x86",
+    iconInfor3: "./asset/img/1.png",
+    infor4: "01",
+    iconInfor4: "./asset/img/3.png",
+  },
+  {
+    name: "nfvo9",
+    infor1: "1vCPU x86",
+    iconInfor1: "./asset/img/2.png",
+    infor2: "1TB HDD",
+    iconInfor2: "./asset/img/4.png",
+    infor3: "1vCPU x86",
+    iconInfor3: "./asset/img/1.png",
+    infor4: "01",
+    iconInfor4: "./asset/img/3.png",
+  },
+  {
+    name: "nfvo10",
+    infor1: "1vCPU x86",
+    iconInfor1: "./asset/img/2.png",
+    infor2: "1TB HDD",
+    iconInfor2: "./asset/img/4.png",
+    infor3: "1vCPU x86",
+    iconInfor3: "./asset/img/1.png",
+    infor4: "01",
+    iconInfor4: "./asset/img/3.png",
+  },
+  {
+    name: "nfvo11",
+    infor1: "1vCPU x86",
+    iconInfor1: "./asset/img/2.png",
+    infor2: "1TB HDD",
+    iconInfor2: "./asset/img/4.png",
+    infor3: "1vCPU x86",
+    iconInfor3: "./asset/img/1.png",
+    infor4: "01",
+    iconInfor4: "./asset/img/3.png",
+  },
+  {
+    name: "nfvo12",
+    infor1: "1vCPU x86",
+    iconInfor1: "./asset/img/2.png",
+    infor2: "1TB HDD",
+    iconInfor2: "./asset/img/4.png",
+    infor3: "1vCPU x86",
+    iconInfor3: "./asset/img/1.png",
+    infor4: "01",
+    iconInfor4: "./asset/img/3.png",
+  },
+  {
+    name: "nfvo13",
+    infor1: "1vCPU x86",
+    iconInfor1: "./asset/img/2.png",
+    infor2: "1TB HDD",
+    iconInfor2: "./asset/img/4.png",
+    infor3: "1vCPU x86",
+    iconInfor3: "./asset/img/1.png",
+    infor4: "01",
+    iconInfor4: "./asset/img/3.png",
+  },
+  {
+    name: "nfvo14",
+    infor1: "1vCPU x86",
+    iconInfor1: "./asset/img/2.png",
+    infor2: "1TB HDD",
+    iconInfor2: "./asset/img/4.png",
+    infor3: "1vCPU x86",
+    iconInfor3: "./asset/img/1.png",
+    infor4: "01",
+    iconInfor4: "./asset/img/3.png",
+  },
+  {
+    name: "nfvo15",
+    infor1: "1vCPU x86",
+    iconInfor1: "./asset/img/2.png",
+    infor2: "1TB HDD",
+    iconInfor2: "./asset/img/4.png",
+    infor3: "1vCPU x86",
+    iconInfor3: "./asset/img/1.png",
+    infor4: "01",
+    iconInfor4: "./asset/img/3.png",
+  },
+  {
+    name: "nfvo16",
+    infor1: "1vCPU x86",
+    iconInfor1: "./asset/img/2.png",
+    infor2: "1TB HDD",
+    iconInfor2: "./asset/img/4.png",
+    infor3: "1vCPU x86",
+    iconInfor3: "./asset/img/1.png",
+    infor4: "01",
+    iconInfor4: "./asset/img/3.png",
+  },
+  {
+    name: "nfvo17",
+    infor1: "1vCPU x86",
+    iconInfor1: "./asset/img/2.png",
+    infor2: "1TB HDD",
+    iconInfor2: "./asset/img/4.png",
+    infor3: "1vCPU x86",
+    iconInfor3: "./asset/img/1.png",
+    infor4: "01",
+    iconInfor4: "./asset/img/3.png",
+  },
+  {
+    name: "nfvo18",
+    infor1: "1vCPU x86",
+    iconInfor1: "./asset/img/2.png",
+    infor2: "1TB HDD",
+    iconInfor2: "./asset/img/4.png",
+    infor3: "1vCPU x86",
+    iconInfor3: "./asset/img/1.png",
+    infor4: "01",
+    iconInfor4: "./asset/img/3.png",
+  },
+  {
+    name: "nfvo19",
+    infor1: "1vCPU x86",
+    iconInfor1: "./asset/img/2.png",
+    infor2: "1TB HDD",
+    iconInfor2: "./asset/img/4.png",
+    infor3: "1vCPU x86",
+    iconInfor3: "./asset/img/1.png",
+    infor4: "01",
+    iconInfor4: "./asset/img/3.png",
+  },
+  
+  
+  
+  
 
 ]
+let perPage = 4;
+  let currentPage = 1;
+  let start = 0;
+  let end = perPage;
+  const totalPages = Math.ceil(cards.length / perPage);
+  // console.log(totalPages);
+  const btnNext = document.querySelector('.btn-next');
+  const btnPrev = document.querySelector('.btn-prev');
+  const btnActive = document.querySelector('.btn-active');
+  const active = document.querySelector('.active')
+
+
+  function getCurrentPage(currentPage){
+    start = (currentPage - 1) * perPage;
+    end = currentPage * perPage;
+    // console.log(start, end);
+  }
 function add_child(items){
 
-    var divGroup1158 = document.createElement('div');
-    divGroup1158.setAttribute("class", "group1158")
-    document.getElementById('myId').appendChild(divGroup1158);
-// **** 1 card
-    var divGroup1158Top = document.createElement("div");
-    divGroup1158Top.setAttribute("class","group1158-top");
-
-    var divGroup1158Mid = document.createElement("div");
-    divGroup1158Mid.setAttribute("class","group1158-mid");
-
-    var divGroup1158Bot = document.createElement("div");
-    divGroup1158Bot.setAttribute("class","group1158-bot");
-
-    // var divGroup1158 = document.querySelector('.group1158');
-    divGroup1158.appendChild(divGroup1158Top);
-    divGroup1158.appendChild(divGroup1158Mid);
-    divGroup1158.appendChild(divGroup1158Bot);
-
-
-// **********************************************************
-    var divRibbon = document.createElement("div");
-    divRibbon.setAttribute("class","ribbon");
-    // var divGroup1158Top = document.querySelector('.group1158-top');
-    divGroup1158Top.appendChild(divRibbon);
-
-    var divTriangleRight = document.createElement("div");
-    divTriangleRight.setAttribute("class","triangle-right");
-    // var divGroup1158Top = document.querySelector('.group1158-top');
-    divGroup1158Top.appendChild(divTriangleRight);
-
-// ************************************************************
-    var divCptChip1 = document.createElement("div");
-    divCptChip1.setAttribute("class","cpt-chip1");
-    // var divGroup1158Mid = document.querySelector('.group1158-mid');
-    divGroup1158Mid.appendChild(divCptChip1);
-
-    var divI1vcpu1 = document.createElement("div");
-    divI1vcpu1.setAttribute("class","i1vcpu1");
-    divGroup1158Mid.appendChild(divI1vcpu1);
-
-    var btn1 = document.createElement('img');
-    btn1.setAttribute("src","./asset/img/2.png");
-    // var divImg1 = document.querySelector('.cpt-chip1');
-    divCptChip1.appendChild(btn1);
-
-
-// ************************************************************
-    var divCptHd = document.createElement("div");
-    divCptHd.setAttribute("class","cpt-hd");
-    divGroup1158Mid.appendChild(divCptHd);
-
-    var divI1tbhdd = document.createElement("div");
-    divI1tbhdd.setAttribute("class","i1tbhdd");
-    divGroup1158Mid.appendChild(divI1tbhdd);
-
-    
-    var btn2 = document.createElement('img');
-    btn2.setAttribute("src","./asset/img/4.png");
-    // var divImg2 = document.querySelector('.cpt-hd');
-    divCptHd.appendChild(btn2);
-
-    
-// ************************************************************
-    var divCptRam1 = document.createElement("div");
-    divCptRam1.setAttribute("class","cpt-ram1");
-    // var divGroup1158Bot = document.querySelector('.group1158-bot');
-    divGroup1158Bot.appendChild(divCptRam1);
-
-    var divI1vcpu2 = document.createElement("div");
-    divI1vcpu2.setAttribute("class","i1vcpu2");
-    divGroup1158Bot.appendChild(divI1vcpu2);
-
-    
-
-    var btn3 = document.createElement('img');
-    btn3.setAttribute("src","./asset/img/1.png");
-    // var divImg3 = document.querySelector('.cpt-ram1');
-    divCptRam1.appendChild(btn3);
-
-// ************************************************************
-    var divCptMonitor = document.createElement("div");
-    divCptMonitor.setAttribute("class","cpt-monitor");
-    divGroup1158Bot.appendChild(divCptMonitor);
-
-    var divI04 = document.createElement("div");
-    divI04.setAttribute("class","i04");
-    divGroup1158Bot.appendChild(divI04);
-
-    var btn4 = document.createElement('img');
-    btn4.setAttribute("src","./asset/img/3.png");
-    // var divImg4 = document.querySelector('.cpt-monitor');
-    divCptMonitor.appendChild(btn4);
-
-    // ************************************************************
-    var para = document.createElement("p");
-    var x = document.createTextNode(`${items.name}`)
-    para.appendChild(x);
-    para.style.margin = "0";
-    // var div = document.querySelector(".ribbon");
-    divRibbon.appendChild(para);
-
-    var para1 = document.createElement("p");
-    var x1 = document.createTextNode(`${items.infor1}`);
-    para1.appendChild(x1);
-    para1.style.color = "white";
-    // var div1 = document.querySelector(".i1vcpu1");
-    divI1vcpu1.appendChild(para1);
-
-    var para2 = document.createElement("p");
-    var x2 =document.createTextNode(`${items.infor2}`)
-    para2.appendChild(x2);
-    para2.style.color = "white";
-    // var div2 = document.querySelector(".i1tbhdd");
-    divI1tbhdd.appendChild(para2);
-
-    var para3 = document.createElement("p");
-    var x3 = document.createTextNode(`${items.infor3}`);
-    para3.appendChild(x3);
-    para3.style.color = "white";
-    // var div3 = document.querySelector(".i1vcpu2");
-    divI1vcpu2.appendChild(para3);
-    
-    var para4 = document.createElement("p");
-    var x4 =document.createTextNode(`${items.infor4}`)
-    para4.appendChild(x4);
-    para4.style.color = "white";
-    // var div4 = document.querySelector(".i04");
-    divI04.appendChild(para4);
+    html = "";
+    const content = cards.map((item, index) => {
+      if (index >= start && index < end) {
+        html += '<div class="group1158">';
+        html += '<div class="group1158-top">';
+        html += '<div class="ribbon">';
+        html += '<p class="nfv_number" style="margin: 0px;">' + item.name + "</p>";
+        html += "</div>";
+        html += '<div class="triangle-right">';
+        html += "</div>";
+        html += "</div>";
+        html += '<div class="group1158-mid">';
+        html += '<div class="cpt-chip1">';
+        html += "<img src=" + item.iconInfor1 + ">";
+        html += "</div>";
+        html += '<div class="i1vcpu1">';
+        html += '<p style="color: white;">' + item.infor1 + "</p>";
+        html += "</div>";
+        html += '<div class="cpt-hd">';
+        html += "<img src= " + item.iconInfor2 + ">";
+        html += "</div>";
+        html += '<div class="i1tbhdd">';
+        html += '<p style="color: white;">' + item.infor2 + "</p>";
+        html += "</div>";
+        html += "</div>";
+        html += '<div class="group1158-bot">';
+        html += '<div class="cpt-ram1">';
+        html += "<img src=" + item.iconInfor3 + ">";
+        html += "</div>";
+        html += '<div class="i1vcpu2">';
+        html += '<p style="color: white;">' + item.infor3 + "</p>";
+        html += "</div>";
+        html += '<div class="cpt-monitor">';
+        html += "<img src=" + item.iconInfor3 + ">";
+        html += "</div>";
+        html += '<div class="i04">';
+        html += '<p style="color: white;">' + item.infor4 + "</p>";
+        html += "</div>";
+        html += "</div>";
+        html += "</div>";
+        return html;
+      }
+  
+    });
+    document.getElementById("myId").innerHTML = html;
 
 }
+renderListPage();
+  function renderListPage(){
+    let html = '';
+    html += `<li class="active"> <a>${1}</a></li>`;
+    for(let i = 2; i <= totalPages; i++){
+        html += `<li><a>${i}</a></li>`
+    }
+
+    document.getElementById("number-page").innerHTML = html;
+  }
+  
+
+  function changPage(){
+    const currentPages = document.querySelectorAll('.number-page li');
+    console.log(currentPages)
+
+    for(let i = 0; i < currentPages.length; i++){
+      currentPages[i].addEventListener('click', ()=> {
+        let value = i+1;
+        currentPage = value;
+        document.querySelector('.active').classList.remove('active');
+        currentPages[i].classList.add('active');
+        if(currentPage > 1 && currentPage < totalPages){
+          btnPrev.classList.remove('btn-active');
+          btnNext.classList.remove('btn-active');
+        }
+        if(currentPage === 1){
+          btnPrev.classList.add('btn-active');
+          btnNext.classList.remove('btn-active');
+        }
+        if(currentPage === totalPages){
+          btnNext.classList.add('btn-active');
+          btnPrev.classList.remove('btn-active');
+        }
+        getCurrentPage(currentPage);
+        add_child()
+      })
+    }
+  }
+
+changPage()
+
+btnNext.addEventListener("click", () => {
+        currentPage++;
+        if(currentPage > totalPages){
+            currentPage = totalPages;
+        }
+
+        if(currentPage === totalPages){
+          btnNext.classList.add('btn-active');
+        }
+        btnPrev.classList.remove('btn-active');
+        document.querySelector('.active').classList.remove('active')
+        document.querySelectorAll('.number-page li')[(currentPage - 1)].classList.add('active')
+        
+        
+        getCurrentPage(currentPage)
+        add_child()
+    })
+
+btnPrev.addEventListener("click", () => {
+    currentPage--;
+    if(currentPage <= 1){
+        currentPage = 1;
+    }
+    if(currentPage === 1) {
+      btnPrev.classList.add('btn-active');
+    }
+    btnNext.classList.remove('btn-active');
+    document.querySelector('.active').classList.remove('active')
+    document.querySelectorAll('.number-page li')[(currentPage - 1)].classList.add('active')
+    getCurrentPage(currentPage)
+    add_child()
+})
 function some(){
-    for (let i = 0; i <arr.length;i++){
-        add_child(arr[i]);
+    for (let i = 0; i <cards.length;i++){
+        add_child(cards[i]);
     }
 }
+
+// JS Search
+
+const searchButton = document.querySelector('.searchButton');
+
+let elements = document.getElementsByClassName('group1158');
+let myId = document.getElementById('myId')
+let myIdNote = document.querySelector('.myId-note');
+const searchInput = document.querySelector('.searchInput');
+let valueItem = searchInput.value;
+
+function mySearch(){
+    // let myId = document.getElementById('myId')
+    const searchInput = document.querySelector('.searchInput');
+    let valueItem = searchInput.value;
+    let k = 0;
+    
+    Array.from(elements).forEach(function(ele){
+        let nameItem = ele.querySelector('.nfv_number').textContent;
+
+        if (nameItem.indexOf(valueItem) !== -1) {
+            ele.style.display = 'block';
+            k++;
+            myIdNote.style.display= 'none';
+        } else {
+            ele.style.display = 'none';
+        }
+        if(k === 0) {
+            myIdNote.style.display= 'block';
+            myIdNote.textContent = `Không tồn tại kết quả "${valueItem}"`
+            myIdNote.style.fontSize = '40px'
+            myIdNote.style.color = 'white'
+            } else {
+                myIdNote.textContent = ``
+            } 
+    });
+}
+searchInput.addEventListener("keyup", function(event){
+    if(event.keyCode === 13){
+        event.preventDefault();
+        searchButton.click();
+    }
+})
+// Languages
+
